479d12aed46fbae17493816a1052d65c67d37212
Mohamed-Amine Bouqsimi <mbouqsim@redhat.com>
2022-07-25 10:00:32 +0200

    update script

--- momentum/momentum.py
+++ momentum/momentum.py
@@ -2,25 +2,35 @@ import yfinance as yf
 import pandas as pd
 from ta import add_all_ta_features
 from ta.utils import dropna
-from catboost import CatBoostClassifier
 import math
 import numpy as np
 from lightgbm import LGBMClassifier
 import argparse
+from sklearn.preprocessing import StandardScaler
 
+
+class renamer():
+    def __init__(self):
+        self.d = dict()
+
+    def __call__(self, x):
+        if x not in self.d:
+            self.d[x] = 0
+            return x
+        else:
+            self.d[x] += 1
+            return "%s_%d" % (x, self.d[x])
 
 def to_signal(returns):
-    if returns >= 0.03:
+    if returns >= 0.04:
         return 1
-    if returns <= 0.0:
-        return -1
     return 0
 
 def std_x_y(df):
-    #scaler = StandardScaler()
+    scaler = StandardScaler()
     X = df.drop(columns=['signal']).to_numpy()
-    #scaler.fit(X)
-    #X = scaler.fit_transform(X)
+    scaler.fit(X)
+    X = scaler.fit_transform(X)
     Y = df['signal'].to_numpy()
     return X, Y
 
@@ -40,6 +50,10 @@ def backtest(test_df):
 
     test_arr = test_df[['Close', 'signal_pred']].reset_index().to_numpy()
 
+    entry_price = 0
+    winers = 0
+    total = 0
+
     for idx, data in enumerate(test_arr):
         date, price, sig = data[0].date(), data[1], data[2]
 
@@ -51,18 +65,27 @@ def backtest(test_df):
             quant = math.floor(capital/price)
             shares = quant
             capital = capital - shares * price
+            entry_price = price
 
         if shares > 0:
             hold_duration += 1
-            if sig == -1 or hold_duration >= max_hold or idx == len(test_arr)-1:
+            if hold_duration >= max_hold or idx == len(test_arr)-1:
+                hold_duration = 0
+                # sell
                 capital = capital + shares * price
                 shares = 0
+                total += 1
+                if price > entry_price:
+                    winers +=1
+                    entry_price = 0
         
-    print('returns_backtest: {}'.format((capital-ini_capital)/ini_capital))
+    print('backtest_returns: {}'.format((capital-ini_capital)/ini_capital))
+    print('backtest_win_trades: {}'.format(winers))
+    print('backtest_win_rate: {}'.format(winers/total))
 
 def get_stock_data(sym):
-    ticker = yf.Ticker(sym)  #PRX.AS ASML.AS
-    raw_df = ticker.history(start='2010-01-01', end="2022-01-01", interval="1d")
+    ticker = yf.Ticker(sym)
+    raw_df = ticker.history(start='2006-01-01', end="2022-01-01", interval="1d")
 
     raw_df['returnsM'] = raw_df[['Close']].pct_change(periods=30)
     df = raw_df[['Open', 'High', 'Low', 'Close', 'Volume', 'returnsM']]
@@ -71,7 +94,7 @@ def get_stock_data(sym):
     df.drop(columns=['returnsM', 'returnsM_shift'], inplace=True)
 
     # Clean NaN values
-    df = dropna(df)
+    df = df.dropna()
 
     # Add all ta features
     df = add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume")
@@ -79,9 +102,21 @@ def get_stock_data(sym):
     df = df.fillna(method='backfill')
     df = pd.concat([df[['Open', 'High', 'Low', 'Close', 'Volume', 'signal']], df.loc[:,df.columns.str.startswith('momentum')|df.columns.str.startswith('trend')]], join = 'outer', axis = 1)
 
-    train_df = df.loc[:'2016-01-01']
-    val_df = df.loc['2016-06-01':'2019-01-01']
-    test_df = df.loc['2019-01-01':'2021-11-01']
+    # remove features
+    df = df.loc[:,~df.columns.str.startswith('trend_vortex_') 
+        & ~df.columns.str.startswith('trend_psar') & ~df.columns.str.startswith('trend_ichimoku') 
+        & ~df.columns.str.startswith('momentum_ppo') & ~df.columns.str.startswith('momentum_stoch') 
+        & ~df.columns.str.startswith('trend_aroon') & ~df.columns.str.startswith('momentum_wr') 
+        & ~df.columns.str.startswith('trend_ema') & ~df.columns.str.startswith('momentum_ao')]
+    
+    # add previous 5 days data
+    tmp = df.drop(columns=['signal'])
+    df = pd.concat([df, tmp.shift(), tmp.shift(2), tmp.shift(3), tmp.shift(4)], axis=1).dropna()
+    df = df.rename(columns=renamer())
+
+    train_df = df.loc[:'2016-07-01']
+    val_df = df.loc['2016-07-01':'2019-07-01']
+    test_df = df.loc['2019-07-01':'2022-07-01']
 
     return train_df, val_df, test_df
 
@@ -95,11 +130,11 @@ if __name__=="__main__":
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--n_estimators', type=int, default=10000,
                         help='the number of estimators')
-    parser.add_argument('--learning_ate', type=float, default=0.005,
+    parser.add_argument('--learning_ate', type=float, default=0.01,
                     help='the learning rate') 
-    parser.add_argument('--num_leaves', type=int, default=52,
+    parser.add_argument('--num_leaves', type=int, default=128,
                     help='number of leaves')
-    parser.add_argument('--max_depth', type=int, default=12,
+    parser.add_argument('--max_depth', type=int, default=24,
                 help='max depth')
     args = parser.parse_args()
 
@@ -117,20 +152,13 @@ if __name__=="__main__":
         'subsample': 1
     }
 
-    #clf = CatBoostClassifier(
-    #    iterations=3000, 
-    #    learning_rate=0.08,
-        #custom_loss=['AUC', 'Accuracy']
-    #)
-
-
     clf = LGBMClassifier(**params)
 
     x_train, y_train = [], []
     x_val, y_val = [], []
     x_test, y_test = [], []
 
-    symbols = ['TSLA', 'NFLX', 'AMD']
+    symbols = ['TSLA', 'NFLX', 'AMD', 'QQQ']
 
     tests = {}
 
